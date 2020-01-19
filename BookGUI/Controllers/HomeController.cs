cd4a07d9e67445ac0a4b881d603e0f506827411f
KosmoKss77 <adrian.k960607@gmail.com>
2020-01-19 22:29:22 +0100

    Last Controller and ViewModel for index action

---
+++ BookGUI/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookGUI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookGUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookRepositoryGUI _bookRepository;
+        private IAuthorRepositoryGUI _authorRepository;
+        private ICountryRepositoryGUI _countryRepository;
+        private ICategoryRepositoryGUI _categoryRepository;
+        private IReviewerRepositoryGUI _reviewerRepository;
+        private IReviewRepositoryGUI _reviewRepository;
+
+        public HomeController(
+            IBookRepositoryGUI bookRepository,
+            IAuthorRepositoryGUI authorRepository, 
+            ICountryRepositoryGUI countryRepository, 
+            ICategoryRepositoryGUI categoryRepository, 
+            IReviewerRepositoryGUI reviewerRepository, 
+            IReviewRepositoryGUI reviewRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _countryRepository = countryRepository;
+            _categoryRepository = categoryRepository;
+            _reviewerRepository = reviewerRepository;
+            _reviewRepository = reviewRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookGUI/Services/BookRepositoryGUI.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BookApiProject.Dtos;
+
+namespace BookGUI.Services
+{
+    public class BookRepositoryGUI : IBookRepositoryGUI
+    {
+        public BookDto GetBookById(int bookId)
+        {
+            BookDto book = new BookDto();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60039/api/");
+                var response = client.GetAsync($"books/{bookId}");
+                response.Wait();
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var readTask = response.Result.Content.ReadAsAsync<BookDto>();
+                    readTask.Wait();
+                    book = readTask.Result;
+                }
+            }
+            return book;
+        }
+
+        public BookDto GetBookByIsbn(string bookIsbn)
+        {
+            BookDto book = new BookDto();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60039/api/");
+                var response = client.GetAsync($"books/ISBN/{bookIsbn}");
+                response.Wait();
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var readTask = response.Result.Content.ReadAsAsync<BookDto>();
+                    readTask.Wait();
+                    book = readTask.Result;
+                }
+            }
+            return book;
+        }
+
+        public decimal GetBookRating(int bookId)
+        {
+            decimal rating = 0.0m;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60039/api/");
+                var response = client.GetAsync($"books/{bookId}/rating");
+                response.Wait();
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var readTask = response.Result.Content.ReadAsAsync<decimal>();
+                    readTask.Wait();
+                    rating = readTask.Result;
+                }
+            }
+            return rating;
+        }
+
+        public IEnumerable<BookDto> GetBooks()
+        {
+            IEnumerable<BookDto> books = new List<BookDto>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60039/api/");
+                var response = client.GetAsync("books");
+                response.Wait();
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var readTask = response.Result.Content.ReadAsAsync<IList<BookDto>>();
+                    readTask.Wait();
+                    books = readTask.Result;
+                }
+            }
+            return books;
+        }
+    }
+}
