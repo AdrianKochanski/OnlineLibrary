72e05b002ee241b45a3d88c4bd5a105b7219840d
KosmoKss77 <adrian.k960607@gmail.com>
2020-01-21 19:51:37 +0100

    New: Edit and Add actions with views in CountriesController

--- BookGUI/Controllers/CountriesController.cs
+++ BookGUI/Controllers/CountriesController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BookGUI.ViewModels;
+using BookApiProject.Models;
+using System.Net.Http;
 
 namespace BookGUI.Controllers
 {
@@ -55,7 +57,81 @@ namespace BookGUI.Controllers
                 Country = country
             };
 
+            ViewBag.SuccessMessage = TempData["SuccessMessage"];
             return View(countryAuthors);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCountry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCountry(Country country)
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60039/api/");
+                var responseTask = client.PostAsJsonAsync("countries", country);
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var newCountryTask = result.Content.ReadAsAsync<Country>();
+                    newCountryTask.Wait();
+                    var newCountry = newCountryTask.Result;
+                    TempData["SuccessMessage"] = $"Country {newCountry.Name} was successfully created.";
+                    return RedirectToAction("GetCountryById", new { countryId = newCountry.Id });
+                }
+                if((int)result.StatusCode == 422)
+                {
+                    ModelState.AddModelError("", "Country Already Exists!");
+                } else {
+                    ModelState.AddModelError("", "Some kind of error. Country not created!");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCountry(int countryId)
+        {
+            var countryToUpdate = _countryRepository.GetCountryById(countryId);
+            if(countryToUpdate == null)
+            {
+                ModelState.AddModelError("", "Error getting country");
+                countryToUpdate = new CountryDto();
+            }
+            return View(countryToUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCountry(CountryDto countryToUpdate)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60039/api/");
+                var responseTask = client.PutAsJsonAsync($"countries/{countryToUpdate.Id}", countryToUpdate);
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = $"Country {countryToUpdate.Name} was successfully updated.";
+                    return RedirectToAction("GetCountryById", new { countryId = countryToUpdate.Id });
+                }
+                if ((int)result.StatusCode == 422)
+                {
+                    ModelState.AddModelError("", "Country Already Exists!");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Some kind of error. Country not created!");
+                }
+            }
+            var countryDto = _countryRepository.GetCountryById(countryToUpdate.Id);
+            return View(countryDto);
         }
     }
 }
